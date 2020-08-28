2d4f7a3330b6e1d2e8f01b72c3e5ee48c83d6ff4
pallivishal <pallivishal2013@gmail.com>
2020-08-28 16:40:00 +0530

    added view-port and scale = 1 in meta data at head.

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
   <head>
     <meta charset="utf-8">
     <title>Hello World!</title>
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/master.css">
   </head>
